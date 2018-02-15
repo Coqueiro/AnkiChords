460d0aaec9a4d12ed13c6a62884149c62ad2f8d8
Lucas Garcia <kartgarcia@gmail.com>
2018-02-15 20:14:55 -0200

    adding new sample
    This sample has some missing name chords

--- sample.py
+++ sample.py
@@ -2,5 +2,6 @@ from tools import convert_music
 
 for title in ['Barfuß am Klavier',
               'Can\'t Help Falling In Love',
-              'She Moves In Her Own Way']:
+              'She Moves In Her Own Way',
+              'Imagine']:
     convert_music(title)
